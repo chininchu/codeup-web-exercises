7b85f3ecc67436ae498423428e44b1dd7cf2d7b3
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-03 16:44:42 -0500

    Created an Ajax Function and a done function

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,8 +1,47 @@
 "use strict";
 
-// TO MAKE THE MAP APPEAR YOU MUST
-// ADD YOUR ACCESS TOKEN FROM
-// https://account.mapbox.com
+// Document.ready is required to detect the state of readiness
+
+$(document).ready(function () {
+
+//  Using $Ajax to pull weather conditions in Dallas,TX
+
+    $.ajax({
+
+        url: "https://api.openweathermap.org/data/2.5/forecast",
+        type: "GET",
+        data: {
+            APPID: OPEN_WEATHER_APPID,
+            q: "Dallas, US"
+
+        }
+    }).done(function (data) {
+        console.log(data);
+
+        let weather = ""
+
+        // Start from here
+
+        for (let i = 0; i < data.list.length; i += 8) {
+
+            weather += '<p>'
+            weather += (data.list[i].weather[0].description)
+            weather += (data.list[i].main.humidity)
+            weather += (data.list[i].wind.speed)
+            weather += (data.list[i].main.pressure)
+            weather += '</p>'
+
+
+        }
+
+
+    })
+
+
+});
+
+
+
 
 
 
