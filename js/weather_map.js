895fded38a04403006dd923e86a7ee71f1ca6218
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-04 15:38:30 -0500

    Marker addeed and is able to move within the map

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,53 +2,106 @@
 
 // Document.ready is required to detect the state of readiness
 
-$(document).ready(function () {
+// (document).ready(function () {
 
 //  Using $Ajax to pull weather conditions in Dallas,TX
 
-    $.ajax({
 
-        url: "https://api.openweathermap.org/data/2.5/forecast",
-        type: "GET",
-        data: {
-            APPID: OPEN_WEATHER_APPID,
-            q: "Dallas, US",
-            units: "imperial"
+let weather = ""
 
-        }
+
+$.ajax({
+
+    url: "https://api.openweathermap.org/data/2.5/forecast",
+    type: "GET",
+    data: {
+        APPID: OPEN_WEATHER_APPID,
+        q: "Dallas, US",
+        units: "imperial"
+
+    }
+}).done(function (data) {
+    console.log(data);
+
+
+    // Start from here
+
+    for (let i = 0; i < data.list.length; i += 8) {
+
+        weather += '<ul class="list-group-flush">'
+        weather += '<li class="list-group-item">' + " " + " " + (data.list[i].dt_txt.slice(0, 10)) + "</li>"
+        weather += '<li class="list-group-item">' + " " + " " + (data.list[i].main.temp) + "</li>"
+        weather += '<li class="list-group-item">' + "Description:" + " " + (data.list[i].weather[0].description) + "</li>"
+        weather += '<li class="list-group-item">' + "Humidity:" + " " + (data.list[i].main.humidity) + "</li>"
+        weather += '<li class="list-group-item">' + "Wind:" + " " + (data.list[i].wind.speed) + "</li>"
+        weather += '<li class="list-group-item">' + "Pressure:" + " " + (data.list[i].main.pressure) + "</li>"
+        weather += '</ul>'
+
+
+    }
+
+    $('#weather').html(weather);
+
+})
+
+
+//Add a marker and create a function that allows the user to drag the marker and receive the weather for the particular location
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-96.889571, 32.892301], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
+
+
+// Create a new marker.
+let marker = new mapboxgl.Marker()
+marker.setLngLat([-96.889571, 32.892301]).addTo(map);
+
+
+map.on('click', function (maps) {
+
+
+    marker.setLngLat(maps.lngLat).addTo(map);
+
+
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: maps.lngLat.lat,
+        lon: maps.lngLat.lng,
+        units: "imperial"
     }).done(function (data) {
-        console.log(data);
+        // console.log('5 day forecast', data);
 
-        let weather = ""
+
+        weather = ""
 
         // Start from here
 
         for (let i = 0; i < data.list.length; i += 8) {
 
             weather += '<ul class="list-group-flush">'
-            weather += '<li class="list-group-item">' + " " + " " + (data.list[i].dt_txt.slice(0,10)) + "</li>"
+            weather += '<li class="list-group-item">' + " " + " " + (data.list[i].dt_txt.slice(0, 10)) + "</li>"
             weather += '<li class="list-group-item">' + " " + " " + (data.list[i].main.temp) + "</li>"
             weather += '<li class="list-group-item">' + "Description:" + " " + (data.list[i].weather[0].description) + "</li>"
-            weather += '<li class="list-group-item">' +"Humidity:" + " " + (data.list[i].main.humidity) + "</li>"
-            weather += '<li class="list-group-item">' +"Wind:" + " " + (data.list[i].wind.speed) + "</li>"
-            weather += '<li class="list-group-item">' +"Pressure:" + " " + (data.list[i].main.pressure) + "</li>"
+            weather += '<li class="list-group-item">' + "Humidity:" + " " + (data.list[i].main.humidity) + "</li>"
+            weather += '<li class="list-group-item">' + "Wind:" + " " + (data.list[i].wind.speed) + "</li>"
+            weather += '<li class="list-group-item">' + "Pressure:" + " " + (data.list[i].main.pressure) + "</li>"
             weather += '</ul>'
 
 
         }
-
         $('#weather').html(weather);
 
 
-
-
-
-    })
-
-
+    });
 
 
 });
+
+
 
 
 
