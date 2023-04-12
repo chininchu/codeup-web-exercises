60da934862f62a7d848852fab8d04105a6bd203c
chininchu <120990876+chininchu@users.noreply.github.com>
2023-04-11 20:30:34 -0500

    Final Commit

--- js/promises.js
+++ js/promises.js
@@ -1,14 +1,22 @@
 "use strict";
 
 
+"use strict";
 
-fetch('https://api.github.com/events', {headers: {'Authorization': PROMISE_TOKEN}})
+function dateOfLastCommit(ghUsername) {
+    return fetch(`https://api.github.com/users/${ghUsername}/events/public`, {
+        headers: {
+            'Authorization': `token ${PROMISE_TOKEN}`
+        }
+    })
+        .then(response => response.json())
+        .then(events => {
+            let pushEvents = events.filter(event => event.type === 'PushEvent')
+            return pushEvents[0].created_at
+        });
+}
 
+dateOfLastCommit("andychu").then(data => console.log(new Date(data).toDateString()));
 
-.then(response => {
-
-    console.log(response.json())
-
-});
 
 
